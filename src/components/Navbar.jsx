040a62b92c9d8f9ed15dfecd84cc529e3e88c684
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-31 14:25:41 -0400

    implementing layout/navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import { styles } from "../styles";
+import { navLinks } from "../constants"; //structured about, work, title
+import { logo, menu, close } from "../assets";
+
 const Navbar = () => {
-  return <>Navbar</>;
+  console.log("logo", logo);
+  return (
+    <nav>
+      <div>
+        <Link>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Christian &nbsp;
+            <span className="sm:block hidden"> | Web Developer</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
