48c8ebdb2ef5f7d63f81205b010ee76827af8709
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-09-01 14:35:11 -0400

    install prettier for tailwindCss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,13 @@ import { navLinks } from "../constants"; //structured about, work, title
 import { logo, menu, close, logoWhiteNoBG } from "../assets";
 
 const Navbar = () => {
-  const [acive, setActice] = useState("");
+  const [active, setActive] = useState("");
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5`}>
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+    <nav
+      className={`${styles.paddingX} fixed top-0 z-20 flex w-full items-center py-5`}
+    >
+      <div className="mx-auto flex w-full max-w-7xl items-center justify-between">
         <Link
           to="/"
           className="flex items-center"
@@ -22,13 +24,18 @@ const Navbar = () => {
           <img
             src={logoWhiteNoBG}
             alt="logo"
-            className="w-16 h-16 object-contain"
+            className="h-16 w-16 object-contain"
           />
         </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-10">
+        <ul className="hidden list-none flex-row gap-10 sm:flex">
           {navLinks.map((link) => {
             return (
-              <li key={link.id} className="">
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title ? "text-white" : "text-secondary"
+                } cursor-pointer font-medium hover:text-white`}
+              >
                 {link.title}
               </li>
             );
