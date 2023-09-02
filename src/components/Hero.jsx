3eadb1a1570087515e005aab2ae32b7b44210706
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-09-02 16:53:06 -0400

    implementing Computers component with r3f

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import { styles } from "../styles";
 
+import { ComputerCanvas } from "./canvas";
+// import { Computers } from "./canvas";
+
 const Hero = () => {
   return (
     <section className="relative h-screen">
-      <div id="hero-content" className="absolute inset-0 border border-sky-500">
+      {/*Hero Content */}
+      <div
+        id="hero-content"
+        className={`${styles.paddingX} absolute inset-0 top-[120px] mx-auto flex max-w-7xl gap-5`}
+      >
+        {/*Scroll Down Icon */}
         <div
           id="bar-scroll"
-          className="mt-5 flex flex-col items-center justify-center border border-sky-500"
+          className="mt-5 flex flex-col items-center justify-start"
         >
           <div className="h-5 w-5 rounded-full bg-[#915EFF]" />
           <div className="violet-gradient h-40 w-1 sm:h-80" />
         </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915EFF]">Christian</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop user interfaces
+            <br className="hidden sm:block" /> and web applications
+          </p>
+        </div>
       </div>
+      <ComputerCanvas></ComputerCanvas>
     </section>
   );
 };
