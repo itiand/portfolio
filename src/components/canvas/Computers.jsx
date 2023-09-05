42d222085581866dee85c84e8591562a933526cc
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-09-05 12:33:31 -0400

    adding lights to the scene

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,29 +7,29 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
+    //mesh
+    //hemisphere lights
+    //all sorts of light
+    //primitive, which is the computer?
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
+      <ambientLight />
       <pointLight intensity={1} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
+      <hemisphereLight intensity={0.15} groundColor="black"></hemisphereLight>
     </mesh>
   );
 };
 
 const ComputerCanvas = () => {
-  return <Canvas></Canvas>;
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{
+        position: [20, 3, 5],
+        fov: 25,
+      }}
+    ></Canvas>
+  );
 };
 
 export default ComputerCanvas;
