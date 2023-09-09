c2a54b6f37b90018b76bc076b94698cb132b85f4
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-09-09 14:13:49 -0400

    implementing avatar blinking

--- src/components/canvas/AvatarMe.jsx
+++ src/components/canvas/AvatarMe.jsx
@@ -1,10 +1,30 @@
 import { useGLTF } from "@react-three/drei";
+import { useEffect, useState } from "react";
+import * as THREE from "three";
 
 const Avatar = () => {
   const { scene } = useGLTF("./readyPlayerMe.glb");
 
-  console.log("scene", scene);
+  const [eyeScale, setEyeScale] = useState(new THREE.Vector3(1, 1, 1));
 
+  /////blink////
+  //handle
+  const handleBlink = () => {
+    setEyeScale(new THREE.Vector3(1, 1, -0.01));
+
+    setTimeout(() => {
+      setEyeScale(new THREE.Vector3(1, 1, 1));
+    }, 50);
+  };
+
+  //useEffect
+  useEffect(() => {
+    const randomBlinkInterval = Math.random() * 3000 + 2000;
+    const blinkInterval = setInterval(handleBlink, randomBlinkInterval);
+    return () => clearInterval;
+  }, []);
+
+  //hide hands
   scene.traverse((child) => {
     if (child.isMesh && child.name === "Wolf3D_Hands") {
       child.visible = false;
