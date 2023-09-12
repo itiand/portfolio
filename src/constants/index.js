83aae8061289766fbe837d589748c90d93f66cee
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-09-12 18:33:58 -0400

    add blue butterfly model

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,10 +29,10 @@ export const navLinks = [
     id: "about",
     title: "About",
   },
-  {
-    id: "work",
-    title: "Work",
-  },
+  // {
+  //   id: "work",
+  //   title: "Work",
+  // },
   {
     id: "contact",
     title: "Contact",
