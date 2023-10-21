83a7eea4a3eb131437343814d32ab1df693d558d
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-10-21 17:13:11 -0400

    comment out scroll indicator

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,13 +13,13 @@ const Hero = () => {
         className={`${styles.paddingX} absolute inset-0 top-[150px] mx-auto flex max-w-7xl gap-5 sm:top-[260px]`}
       >
         {/*Scroll Down Icon */}
-        <div
+        {/* <div
           id="bar-scroll"
           className="mt-5 flex flex-col items-center justify-start"
         >
           <div className="h-5 w-5 rounded-full bg-[#915EFF]" />
           <div className="violet-gradient h-40 w-1 sm:h-80" />
-        </div>
+        </div> */}
         {/* mt-28 */}
         <div className="text-[70px] sm:text-[70px] md:text-[75px] lg:text-[100px] xl:text-[110px]">
           <h1 className={`${styles.heroHeadText} leading-tight text-white`}>
